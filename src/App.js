1f9df5ae5604aa709a4d0029c21d84aa84317465
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-11-22 10:56:28 +0200

    Add possibility users to login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { Login } from './components/auth/Login';
 import { Register } from './components/auth/Register';
 import { Chat } from './components/chat/Chat';
+import { AuthContextProvider } from './context/AuthContext';
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <AuthContextProvider>
+      <div className="App">
+        <Login />
+      </div>
+    </AuthContextProvider>
   );
 }
 
--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,6 +1,36 @@
+import { setPersistence, signInWithEmailAndPassword, browserSessionPersistence } from 'firebase/auth';
+import { AuthContext } from '../../context/AuthContext';
+import { useContext } from 'react';
+
 export const Login = () => {
+    const { auth } = useContext(AuthContext);
+
+    const onLogin = (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.target);
+        const email = formData.get('email');
+        const password = formData.get('password');
+
+        if (email === '' || password === '') {
+            alert('Please fill all the fields!');
+            return;
+        }
+
+        setPersistence(auth, browserSessionPersistence)
+            .then(() => {
+                signInWithEmailAndPassword(auth, email, password)
+                    .then(() => {
+                        console.log('logged');
+                    })
+                    .catch((err) => {
+                        alert(err.message);
+                    })
+            })
+    }
+
     return (
-        <form className='auth'>
+        <form className='auth' onSubmit={onLogin}>
             <h3>Login Here</h3>
             <label htmlFor="email">Username</label>
             <input type="text" placeholder="Email" id="email" name="email" />
