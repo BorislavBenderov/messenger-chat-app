6f8d20e2d51a2f24807a05a7e0b8f20842e32c8f
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-14 09:09:37 +0200

    Add hash router

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'font-awesome/css/font-awesome.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
+    <HashRouter>
         <App />
-    </BrowserRouter>
+    </HashRouter>
 );
